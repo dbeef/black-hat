7ee04a920eccd6b5ffde0475839bdf4c374c88c0
Dan <Dan@192.168.1.3>
2016-01-10 00:20:58 +0100

    First Commit A brand new project

---
+++ Black Hat-desktop/src/com/dotgames/blackhat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dotgames.blackhat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dotgames.blackhat.Starter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Starter(), config);
+	}
+}
